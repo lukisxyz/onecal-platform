e837792b340595476ea921088fc1e6ad9499ac5d
Fahmi <fahmilukis04@gmail.com>
2026-01-27 18:23:27 +0700

    fix(deps): add nitro

--- vite.config.ts
+++ vite.config.ts
@@ -4,6 +4,7 @@ import { tanstackStart } from '@tanstack/react-start/plugin/vite'
 import viteReact from '@vitejs/plugin-react'
 import viteTsConfigPaths from 'vite-tsconfig-paths'
 import { fileURLToPath, URL } from 'url'
+import { nitro } from 'nitro/vite'
 
 import tailwindcss from '@tailwindcss/vite'
 
@@ -22,6 +23,7 @@ const config = defineConfig({
     tailwindcss(),
     tanstackStart(),
     viteReact(),
+    nitro(),
   ],
 })
 
